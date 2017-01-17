9da7254e655710eec963d63a2c360f4590dbba75
michaelzoidl <github@michaelzoidl.com>
2017-01-17 22:29:32 +0100

    Update tests

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import b3m from './lib';
+import b3m from './index';
 
 test('Returns the passed block-class and build a class-string', t => {
   const cn = b3m('button')
@@ -19,4 +19,30 @@ test('Returns class-string with multiple classes if you pass more strings', t =>
 test('Returns class-string with double dashes if string is passed as array', t => {
   const cn = b3m('button')
   t.is(cn(['clickable']), 'button--clickable')
+})
+
+test('Returns passed object into modifier if key is truth', t => {
+  const cn = b3m('button')
+  t.is(cn({clickable: true}), 'button--clickable')
+})
+
+test('Returns passed object without key if prop is false', t => {
+  const cn = b3m('button')
+  t.is(cn({clickable: false}), 'button')
+})
+
+test('Returns passed object without key if prop is false', t => {
+  const cn = b3m('button')
+  t.is(cn({clickable: true}), 'button--clickable')
+})
+
+test('Returns passed object and used key and prop of object since they`re not boolean', t => {
+  const cn = b3m('button')
+  t.is(cn({is: 'hidden'}), 'button--is-hidden')
+})
+
+test('Returns passed object and used key and prop of object and converts the camelCase key into dashes', t => {
+  const cn = b3m('button')
+  t.is(cn({isHidden: true}), 'button--is-hidden')
+  t.is(cn({status: 'isHidden'}), 'button--status-is-hidden')
 })
